7d1716efca9fd7eae8fc151e32468352ad2ef0bd
aca <acadx0@gmail.com>
2021-01-04 01:03:23 +0900

    add error handling, add prefix to msg

--- lua/vidir.lua
+++ lua/vidir.lua
@@ -6,7 +6,13 @@ function ls()
   local line = api.nvim_win_get_cursor(0)
   local cmd = "cut -d '\t' -f2- | xargs -ri -d '\n' ls -alh {}"
   local result = vim.fn.systemlist(cmd, vim.api.nvim_get_current_line())[1]
-  result = vim.fn.substitute(result, '\n', '', 'g')
+
+  if vim.v.shell_error ~= 0 then
+    result = "[vidir] Renamed"
+  else
+    result = "[vidir] " .. vim.fn.substitute(result, '\n', '', 'g')
+  end
+
   api.nvim_buf_set_virtual_text(0, 2, line[1] - 1, {{ result, 'Comment' }}, {})
 end
 
