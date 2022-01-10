0ebc07147c4a6d2cd143b8c8bf193d4c6a6d248e
rok <acadx0@gmail.com>
2022-01-10 16:58:28 +0900

    proper escape filename

--- lua/vidir.lua
+++ lua/vidir.lua
@@ -1,18 +1,18 @@
 local M = {}
 local api = vim.api
 
-function ls()
+local function ls()
   api.nvim_buf_clear_namespace(0, 2, 0, -1)
   local line = api.nvim_win_get_cursor(0)
-  local cmd = "cut -d '\t' -f2- | xargs -ri -d '\n' ls -alh {}"
-  local result = vim.fn.systemlist(cmd, vim.api.nvim_get_current_line())[1]
+  local fname, _ = vim.api.nvim_get_current_line():gsub("^%d*%s*", "")
+  fname = vim.fn.fnameescape(fname)
+  local result = vim.fn.systemlist("ls -al " .. fname, vim.api.nvim_get_current_line())[1]
 
   if vim.v.shell_error ~= 0 then
     result = "[vidir] Renamed"
   else
     result = "[vidir] " .. vim.fn.substitute(result, '\n', '', 'g')
   end
-
   api.nvim_buf_set_virtual_text(0, 2, line[1] - 1, {{ result, 'Comment' }}, {})
 end
 
